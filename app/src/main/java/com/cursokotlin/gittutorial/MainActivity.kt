c42d1fd34ae8d00eacf479ef83fdf32caa5f558a
AristiDevs <aris.guimera@gmail.com>
2022-06-01 19:45:31 +0100

    deleted prints

--- app/src/main/java/com/cursokotlin/gittutorial/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/gittutorial/MainActivity.kt
@@ -7,15 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("Esto es un ejemplo")
-        print("Esto es un ejemplo")
-        print("Esto es un ejemplo")
-        print("Esto es un ejemplo")
-        print("Esto es un ejemplo")
-        print("Esto es un ejemplo")
-        print("Esto es un ejemplo")
-        print("Esto es un ejemplo")
-        print("Esto es un ejemplo")
-        print("Esto es un ejemplo")
     }
 }
